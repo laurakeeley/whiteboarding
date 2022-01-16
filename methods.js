d359c3b3d4d7e9e6be8c08ce184379d5303746c8
laura <l.keeley000@gmail.com>
2022-01-16 10:33:22 -0600

    adds js methods

--- methods.js
+++ methods.js
@@ -1,12 +1,40 @@
 // 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
+// let word = "orange";
+
+// console.log(word.toUpperCase());
+
 // 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+// let num = 302;
+
+// if (num > 100) {
+//   console.log("That's a big number");
+// }
 
 // 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
 
+// let num1 = 5;
+// let num2 = 7;
+
+// console.log(num1 + num2);
+
 // 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
+// let word = "purplehippo";
+
+// console.log(word.split("").reverse().join(""));
+
 // 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+// let num = 8;
+
+// let i = 0;
+
+// while (i < 11) {
+//   console.log(num);
+//   i++;
+// }
 
 // 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
