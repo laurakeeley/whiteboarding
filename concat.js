0c312f0191078c2bf91eb6434749eefbfe2f0877
laura <l.keeley000@gmail.com>
2022-01-15 19:59:11 -0600

    adds js concatenation and interpolation

--- concat.js
+++ concat.js
@@ -1,12 +1,42 @@
 // 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
+let firstName = "Laura";
+let lastName = "Keeley";
+
+console.log(firstName + " " + lastName);
+
 // 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+let nameOne = "Laura";
+let nameTwo = "Keeley";
+
+console.log(`${nameOne} ${nameTwo}`);
 
 // 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
+// var word = window.prompt("Enter a word:");
+
+// if (word === "marco") {
+//   console.log("polo");
+// }
+
 // 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
-// 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+let color1 = "blue";
+let color2 = "yellow";
+let color3 = "green";
+
+console.log(
+  "When you mix " + color1 + " with " + color2 + " you get " + color3
+);
+
+// 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the ${} operator).
+
+let color4 = "blue";
+let color5 = "yellow";
+let color6 = "green";
+
+console.log(`${color4} and ${color5} make ${color6}`);
 
 // 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
