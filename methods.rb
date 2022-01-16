077edfd4f2b24d202d7d6dc598bf58c9867bb502
laura <l.keeley000@gmail.com>
2022-01-16 10:15:30 -0600

    adds ruby methods

---
+++ methods.rb
@@ -0,0 +1,96 @@
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+# puts "Enter a word:"
+
+# word = gets.chomp.upcase
+
+# puts word
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+# puts "Enter a number:"
+
+# number = gets.chomp.to_i
+
+# if number > 100
+#   puts "That's a big number!"
+# end
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+# puts "Enter a number: "
+
+# num1 = gets.chomp.to_i
+
+# puts "Enter another number:"
+
+# num2 = gets.chomp.to_i
+
+# total = num1 + num2
+# p total
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+# puts "Enter a word: "
+
+# word = gets.chomp
+
+# puts word.reverse
+
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+# puts "Enter a number:"
+
+# number = gets.chomp
+
+# 10.times do 
+#   puts number
+# end
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+# puts "Enter a word: "
+# word1 = gets.chomp
+# puts "Enter another word: "
+# word2 = gets.chomp
+
+# puts word1.upcase + word2.upcase
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# puts "Enter a word:"
+
+# word = gets.chomp
+
+# puts word.length
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+# puts "Enter a nubmer: "
+
+# num = gets.chomp.to_i
+
+# if num < 0
+#   puts "That's a negative number!"
+# end
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+# puts "Enter a number: "
+# num1 = gets.chomp.to_i
+# puts "Enter another number: "
+# num2 = gets.chomp.to_i
+
+# puts num1 * num2
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+# puts "Enter a word: "
+
+# word = gets.chomp
+
+# if word.length > 5
+#   puts "That's a long word"
+# end
+
+# SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
