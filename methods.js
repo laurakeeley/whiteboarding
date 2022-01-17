b6dcfb0c4cc8f275561db1e58e3e8a2051879a28
laura <l.keeley000@gmail.com>
2022-01-16 18:28:18 -0600

    add js methods

--- methods.js
+++ methods.js
@@ -38,12 +38,36 @@
 
 // 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
+// let word1 = "taco";
+// let word2 = "cat";
+
+// console.log(word1.toUpperCase() + word2.toUpperCase());
+
 // 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+// let word = "tacocat";
+// console.log(word.length);
 
 // 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
 
+// let num = -8;
+// if (num < 0) {
+//   console.log("that's a negative number!");
+// }
+
 // 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
 
+// let num1 = 7;
+// let num2 = 10;
+
+// console.log(num1 * num2);
+
 // 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+let word = "orange";
+
+if (word.length > 5) {
+  console.log("that's a long word");
+}
 
 // SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
