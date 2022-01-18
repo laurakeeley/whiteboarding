38801eeb22d8a0a8ca2a2783f6bf8828d05e1b49
laura <l.keeley000@gmail.com>
2022-01-18 17:53:07 -0600

    finishes js arrays

--- array.js
+++ array.js
@@ -7,22 +7,71 @@
 
 // 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
-let array = [];
+// let array = ["a", "c", "k", "d"];
+// array[1] = 2;
+
+// console.log(array);
 
 // 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
+// let array = [1, 4, 5, 6, 7];
+// let i = 0;
+// while (i < array.length) {
+//   console.log(array[i]);
+//   i++;
+// }
+
 // 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+// let array = [1];
+// array.push(3, 5, 7);
+
+// console.log(array);
 
 // 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
 
+// let array = ["dog", "cat", "gerbil"];
+// array[2] = array[2].toUpperCase();
+
+// console.log(array);
+
 // 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+// let array = ["Ruby", "Jack", "Carson"];
+
+// let i = 0;
+// while (i < array.length) {
+//   console.log(array[i]);
+//   i++;
+// }
 
 // 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 
+// let array = ["dog", "cat"];
+// array.push("gerbil");
+
+// console.log(array);
+
 // 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+// let array = [4, 6, 2, 4, 5];
+// array[0] = array[0] * 10;
+// console.log(array);
 
 // 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
+// let array = [1, 3];
+// let i = 0;
+// while (i < array.length) {
+//   console.log(array[i]);
+//   i++;
+// }
+
 // 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+let countries = ["US", "Canada", "Mexico"];
+countries.push("Ireland");
+
+console.log(countries);
 
 // SOLUTIONS: https://gist.github.com/peterxjang/7095a2b19e1da2cc18d4a0dcd66cb8f1
