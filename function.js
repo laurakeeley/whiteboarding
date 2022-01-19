1c6979f1a58f3427136c3618a2c16b66817b1f10
laura <l.keeley000@gmail.com>
2022-01-18 21:39:41 -0600

    adds 1-7 js functions

--- function.js
+++ function.js
@@ -1,16 +1,57 @@
 // 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
+// function double(num) {
+//   console.log(num * 2);
+// }
+
+// double(4);
+
 // 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+// function capital(string) {
+//   console.log(string.toUpperCase());
+// }
+
+// capital("laura");
 
 // 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
 
+// function subtract(x, y) {
+//   console.log(x - y);
+// }
+// subtract(5, 3);
+
 // 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+// function multiply(num) {
+//   console.log(num * num);
+// }
+
+// multiply(5);
 
 // 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
 
+// function firstLetter(string) {
+//   console.log(string[0]);
+// }
+
+// firstLetter("monstera");
+
 // 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
 
+// function threeStrings(word1, word2, word3) {
+//   console.log(`${word1} ${word2} ${word3}`);
+// }
+
+// threeStrings("cat", "in", "hat");
+
 // 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+// function toString(num) {
+//   console.log(String(num));
+// }
+
+// toString(5);
 
 // 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
 
