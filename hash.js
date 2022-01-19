54a56197236f17c92d1e875b7a65c1f3e2fe056e
laura <l.keeley000@gmail.com>
2022-01-18 21:20:40 -0600

    finishes js objects

--- hash.js
+++ hash.js
@@ -1,21 +1,144 @@
 // 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
 
+// let contact = {
+//   firstName: "Laura",
+//   lastName: "Keeley",
+//   email: "laura@gmail.com",
+// };
+// console.log(contact.firstName);
+// console.log(contact.lastName);
+// console.log(contact.email);
+
 // 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+// let people = [
+//   {
+//     firstName: "Jack",
+//     lastName: "Hadley",
+//   },
+//   {
+//     firstName: "Ruby",
+//     lastName: "Kaye",
+//   },
+//   {
+//     firstName: "Carson",
+//     lastName: "James",
+//   },
+// ];
+
+// console.log(people[0]);
 
 // 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
 
+// let menu = {
+//   burger: 2.5,
+//   fries: 0.5,
+//   shake: 2.0,
+// };
+
+// menu.soda = 1.5;
+
+// console.log(menu);
+
 // 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+// let book = {
+//   title: "be here now",
+//   author: "ram dass",
+//   pages: 200,
+//   language: "English",
+// };
+
+// console.log(book.title);
+// console.log(book.author);
+// console.log(book.pages);
+// console.log(book.language);
 
 // 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
 
+// let books = [
+//   {
+//     title: "be here now",
+//     author: "ram dass",
+//   },
+//   {
+//     title: "you are a badass",
+//     author: "jen sincero",
+//   },
+//   {
+//     title: "lullaby",
+//     author: "chuck paulanik",
+//   },
+// ];
+
+// console.log(books[2].author);
+
 // 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+
+// let states = {
+//   wisconsin: "madison",
+//   minnesota: "minneapolis",
+//   illinois: "chicago",
+// };
+// states.newMexico = "albuquerque";
+
+// console.log(states);
 
 // 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
 
+// let laptop = {
+//   brand: "mac",
+//   model: "macbook",
+//   year: 2015,
+// };
+
+// console.log(laptop.brand);
+// console.log(laptop.model);
+// console.log(laptop.year);
+
 // 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+// let laptop = [
+//   {
+//     brand: "mac",
+//     model: "macbook",
+//     year: 2015,
+//   },
+//   {
+//     brand: "dell",
+//     model: "workbook",
+//     year: 2004,
+//   },
+//   {
+//     brand: "hp",
+//     model: "chromebook",
+//     year: 2020,
+//   },
+// ];
+
+// console.log(laptop[1].model);
 
 // 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
 
+// let word = {
+//   lardaceous: "of, like or pertaining to lard",
+//   quantophrenia: "obsessive reliance on statistics and mathematical results",
+// };
+
+// word.depotentiate = "to deprive or divest of power";
+
+// console.log(word);
+
 // 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
+
+// let shirt = {
+//   brand: "hanes",
+//   color: "red",
+//   size: "medium",
+// };
+
+// console.log(shirt.brand);
+// console.log(shirt.color);
+// console.log(shirt.size);
 
 // SOLUTIONS: https://gist.github.com/peterxjang/d257aec07882d78009bd796ed53f81bb
