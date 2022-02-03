801af2f48a734e96b0588de4e6aa9792cc1d8afd
laura <l.keeley000@gmail.com>
2022-02-03 10:49:37 -0600

    adds solution to second problem in convert-data ja

--- convert-data.js
+++ convert-data.js
@@ -1,20 +1,31 @@
 //  1. Convert an array of arrays into a hash.
 //     For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}.
 
-let numbers = [
-  [1, 3],
-  [8, 9],
-  [2, 16],
-];
-let object = {};
-numbers.forEach((pair) => {
-  object[pair[0]] = pair[1];
-});
+// let numbers = [
+//   [1, 3],
+//   [8, 9],
+//   [2, 16],
+// ];
+// let object = {};
+// numbers.forEach((pair) => {
+//   object[pair[0]] = pair[1];
+// });
 
-console.log(object);
+// console.log(object);
 
 //  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 //     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+
+// let items = [
+//   { id: 1, color: "blue", price: 32 },
+//   { id: 2, color: "red", price: 12 },
+// ];
+// let newItems = {};
+// items.forEach((item) => {
+//   newItems[item.id] = item;
+// });
+
+// console.log(newItems);
 
 //  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 //     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
