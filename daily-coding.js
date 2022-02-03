ec53604e4654ef0fe925212e413b4a81100f78e7
laura <l.keeley000@gmail.com>
2022-02-03 10:50:50 -0600

    adds problem 20 to daily coding problems

--- daily-coding.js
+++ daily-coding.js
@@ -102,3 +102,13 @@
 // 8 = max(2, 7, 8)
 // 8 = max(7, 8, 7)
 // Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results. You can simply print them out as you compute them.
+
+/////Problem 20 [Easy]//////////////////////////////////////////////////////////////////////
+
+// Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
+
+// For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, return the node with value 8.
+
+// In this example, assume nodes with the same value are the exact same node objects.
+
+// Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.
