4e8cbf590a6902afbd918dc74a9450077973d4f1
laura <l.keeley000@gmail.com>
2022-02-04 08:33:20 -0600

    adds solution to problem six

--- nestedLoops.js
+++ nestedLoops.js
@@ -110,6 +110,18 @@ let array = [5, -2, 1, -9, -7, 2, 6];
 //  6. Use a nested loop with two arrays of numbers to create a new array of the sums of each combination of numbers.
 //     For example, [1, 2] and [6, 7, 8] becomes [7, 8, 9, 8, 9, 10].
 
+let array1 = [1, 2];
+let array2 = [6, 7, 8];
+let newArray = [];
+
+array1.forEach((num) => {
+  array2.forEach((number) => {
+    newArray.push(num + number);
+  });
+});
+
+console.log(newArray);
+
 //  7. Use a nested loop with an array of numbers to compute an array with every combination of products from each number.
 //     For example, [2, 8, 3] becomes [4, 16, 6, 16, 64, 24, 6, 24, 9].
 
